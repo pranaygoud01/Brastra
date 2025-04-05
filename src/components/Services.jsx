bd02e534db80bd77401a42ea60c5b6904cc18277
Pranay Goud <131458600+pranaygoud01@users.noreply.github.com>
2025-04-05 14:22:43 +0530

    issue Resolved

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -16,17 +16,17 @@ const Services = () => {
             <img src={chef} className='h-[650px] max-lg:w-full max-lg:h-[400px] w-[50%] object-contain'/>
             <img src={food6} className='h-[400px] max-lg:hidden w-[16%] max-lg:w-full object-cover'/>
             <div className='w-full flex justify-end py-10 pr-72 max-lg:p-4 h-full max-lg:static absolute'>
-                <div className='bg-white/12 max-lg:w-full h-fit flex flex-col py-8 items-center gap-2 w-4/12 rounded-xl'>
+                <div className='bg-white/12 max-lg:w-full h-full flex flex-col py-8 items-center gap-2 w-4/12 rounded-xl'>
                      <img src={logo1} className='w-30 max-lg:h-20 h-30 object-cover'/>
                      <SiCodefresh className='text-5xl max-lg:text-4xl text-red-500'/>
-                     <p className='font-bold font-albert-sans max-lg:text-xl text-2xl'>Fresh Products</p>
-                     <p className='text-md w-9/12 max-lg:text-sm text-center text-neutral-400'>These nutrients support everything from immune function to bone health.</p>
+                     <p className='font-bold font-albert-sans max-lg:text-xl text-xl'>Fresh Products</p>
+                     <p className='text-xs w-9/12 max-lg:text-sm text-center text-neutral-400'>These nutrients support everything from immune function to bone health.</p>
                      <PiChefHat className='text-5xl mt-3  max-lg:text-4xl text-red-500'/>
-                     <p className='font-bold font-albert-sans max-lg:text-xl text-2xl'>Skilled Chefs</p>
-                     <p className='text-md w-9/12 text-center max-lg:text-sm text-neutral-400'>excellent culinary skills, time management expertise, the ability to work</p>
+                     <p className='font-bold font-albert-sans max-lg:text-xl text-xl'>Skilled Chefs</p>
+                     <p className='text-xs w-9/12 text-center max-lg:text-sm text-neutral-400'>excellent culinary skills, time management expertise, the ability to work</p>
                      <IoReceiptSharp className='text-5xl mt-3  max-lg:text-4xl text-red-500'/>
-                     <p className='font-bold font-albert-sans max-lg:text-xl text-2xl'>Unique Recipes</p>
-                     <p className='text-md w-9/12 pb-5 text-center max-lg:text-sm text-neutral-400'>Having an extra visual element around these items on your menu will help attract your guest's </p>
+                     <p className='font-bold font-albert-sans max-lg:text-xl text-xl'>Unique Recipes</p>
+                     <p className='text-xs w-9/12 pb-5 text-center max-lg:text-sm text-neutral-400'>Having an extra visual element around these items on your menu will help attract your guest's </p>
                 </div>
         </div>
         </div>
