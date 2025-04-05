b659e4b5e114b562920e8712a06afef3c0856b01
Pranay Goud <131458600+pranaygoud01@users.noreply.github.com>
2025-04-05 14:13:23 +0530

    footer-update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { RiInstagramFill } from "react-icons/ri";
 import { Link } from '@tanstack/react-router';
 const Footer = () => {
   return (
-    <div className='w-full flex flex-col max-lg:h-fit max-lg:p-5 font-instrument-sans text-white font px-32 py-10 h-[350px] bg-black'>
+    <div className='w-full flex flex-col max-lg:h-fit max-lg:p-5 font-instrument-sans text-white font px-32 py-10 h-fit bg-black'>
         <div className='border-b flex max-lg:flex-col justify-between items-center max-lg:pb-5 max-lg:items-start border-b-white '>
            <div className='flex flex-col  max-lg:flex-row max-lg:justify-between max-lg:w-full max-lg:text-sm gap-2 items-center max-lg:items-center max-lg:pb-4 pb-10'><img src={logo} className='h-30 max-lg:h-20 max-lg:w-30  w-40 object-cover '/>
            <div className='flex flex-col gap-2'>
@@ -20,7 +20,7 @@ const Footer = () => {
             <li><Link to="/">Christmas Dinner</Link></li>
            </ul>
         </div>
-        <div className='flex justify-between max-lg:py-5 items-center py-10'>
+        <div className='flex justify-between max-lg:py-5 items-center pt-10'>
         <p className='max-lg:text-xs'>copyright &copy; 2025 Brastra</p>
         <div className='flex max-lg:text-xl gap-3 text-4xl'>
             <IoLogoFacebook/>
